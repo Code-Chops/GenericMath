4b4fa765b7db5e16e5d750e3e2b0c762a212c03a
Max Bergman <maxbergman@outlook.com>
2022-09-18 23:25:24 +0200

    2.10.3: Added number JSON serialization.

---
+++ GenericMath/Serialization/NumberJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CodeChops.GenericMath.Serialization;
+
+public class NumberJsonConverter<TNumber> : JsonConverter<Number<TNumber>>
+	where TNumber : struct, IComparable<TNumber>, IEquatable<TNumber>, IConvertible
+{
+	private static TypeCode ExpectedTypeCode { get; } = Type.GetTypeCode(typeof(TNumber));
+	private static JsonConverter<TNumber> DefaultConverter { get; } = (JsonConverter<TNumber>)new JsonSerializerOptions().GetConverter(typeof(TNumber));
+	
+	public override Number<TNumber> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		// Check for null values
+		if (reader.TokenType == JsonTokenType.Null) return default;
+
+		if (reader.TokenType != JsonTokenType.Number) throw new JsonException($"Unexpected token found in JSON: {reader.TokenType}. Expected: {JsonTokenType.Number}.");
+
+		var typeCode = Type.GetTypeCode(typeToConvert);
+		if (typeCode != TypeCode.Object && typeCode != ExpectedTypeCode) throw new JsonException($"Can't convert {typeCode} to {ExpectedTypeCode}.");
+
+		var number = DefaultConverter.Read(ref reader, typeof(TNumber), options);
+		return number;
+	}
+
+	public override void Write(Utf8JsonWriter writer, Number<TNumber> number, JsonSerializerOptions options)
+		=> DefaultConverter.Write(writer, number.Value, options);
+}
---
+++ GenericMath/Serialization/NumberJsonConverterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CodeChops.GenericMath.Serialization;
+
+public class NumberJsonConverterFactory : JsonConverterFactory
+{
+	public override bool CanConvert(Type typeToConvert) 
+		=> !typeToConvert.IsAbstract && typeToConvert.IsAssignableTo(typeof(INumber));
+
+	public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+	{
+		var numberType = typeToConvert.GetGenericArguments()[0];
+		var converter = (JsonConverter)Activator.CreateInstance(
+			type: typeof(NumberJsonConverter<>).MakeGenericType(numberType),
+			bindingAttr: BindingFlags.Instance | BindingFlags.Public,
+			binder: null,
+			args: null,
+			culture: null)!;
+
+		return converter;
+	}
+}
